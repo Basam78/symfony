8ad16e30232a31838b4ed2b22e3d68c1db523e2f
samba camara <samba.camara786@gmail.com>
2023-02-08 12:27:17 +0100

    twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,45 @@
 
 namespace App\Controller;
 
+use stdClass;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: "home")]
     public function index(): Response
     {
-        return new Response('Page d\'accueil');
+        $tableau = ['a','b','c'];
+
+        $objet = new stdClass();
+
+        $compte = new stdClass();
+        $compte->login = 'tutu';
+        $compte->password = 'tutu';
+
+        $liste = [
+            ['login'=>'toto', 'password' => 'azerty'],
+            ['login'=>'titi', 'password' => 'qsdfghh'],
+            ['login'=>'tata', 'password' => 'admin'],
+            $compte,
+        ];
+
+        $objet->nom = "Dupont";
+
+    $objet->prenom = "paul";
+
+        dd($objet);
+
+        return $this->render('home/homepage.html.twig',[
+        'titre'=> 'je suis la page d\'accueil',
+        'tableau' => $tableau,
+        'personne' => $objet,
+        'personne' => $liste,
+        
+       ]);
+        
     }
 }
